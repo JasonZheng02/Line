ee7c53a7c89942df7b013faa686a075bc2721421
JasonZheng02 <heydude506@gmail.com>
2020-02-10 15:33:25 -0500

    Added quad 1 and 5

--- draw.py
+++ draw.py
@@ -1,4 +1,16 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    dx = x1 - x0
+    dy = y1 - y0
+    D = 2*dy - dx
+    x = x0
+    y = y0
+
+    while (x<= x1):
+        plot(screen, color, x, y)
+        if (D > 0):
+               y = y + 1
+               D = D - 2*dx
+        x = x + 1
+        D = D + 2*dy
