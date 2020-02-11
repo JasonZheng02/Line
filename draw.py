1e2080c2970a957f3545963d455988eb4dfa06a8
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 02:42:28 -0500

    .

--- draw.py
+++ draw.py
@@ -2,22 +2,20 @@ from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
     x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
-    print(x0,y0,x1,y1)
     if (x0 > x1 and y0 > y1):
         x0, x1, y0, y1 = x1, x0, y1, y0
     A = y1 - y0
     B = (x1 - x0) * -1
     x = x0
     y = y0
-    print(A,B)
     slope = (A / B) * -1
-    # print(slope)
+    print(slope)
 
     if (slope == 0):
         while (x <= x1):
             plot(screen, color, x, y)
             x = x + 1
-            
+
     elif (slope <= 1 and slope > 0):
         D = 2 * A + B
         while (x <= x1):
