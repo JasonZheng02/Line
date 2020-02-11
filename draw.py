c2c1a15e3639e454cedc36731a301ca678666e48
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 02:55:28 -0500

    .

--- draw.py
+++ draw.py
@@ -19,44 +19,44 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 plot(screen, color, x, y)
                 y = y + 1
 
-    slope = (A / B) * -1
-
-    if (slope <= 1 and slope > 0):
-        D = 2 * A + B
-        while (x <= x1):
-            plot(screen, color, x, y)
-            if (D > 0):
-                y = y + 1
-                D = D + 2 * B
-            x = x + 1
-            D = D + 2 * A
-
-    if (slope > 1):
-        D = A + 2 * B
-        while (y <= y1):
-            plot(screen, color, x, y)
-            if (D < 0):
+    else:
+        slope = (A / B) * -1
+        if (slope <= 1 and slope > 0):
+            D = 2 * A + B
+            while (x <= x1):
+                plot(screen, color, x, y)
+                if (D > 0):
+                    y = y + 1
+                    D = D + 2 * B
                 x = x + 1
                 D = D + 2 * A
-            y = y + 1
-            D = D + 2 * B
 
-    if (slope < -1):
-        D = -1 * A + 2 * B
-        while (y <= y1):
-            plot(screen, color, x, y)
-            if (D > 0):
-                x = x - 1
-                D = D - 2 * A
-            y = y + 1
-            D = D + 2 * B
+        elif (slope > 1):
+            D = A + 2 * B
+            while (y <= y1):
+                plot(screen, color, x, y)
+                if (D < 0):
+                    x = x + 1
+                    D = D + 2 * A
+                y = y + 1
+                D = D + 2 * B
 
-    else:
-        D = 2 * A - B
-        while (x <= x1):
-            plot(screen, color, x, y)
-            if (D < 0):
-                y = y - 1
-                D = D - 2 * B
-            x = x + 1
-            D = D + 2 * A
+        elif (slope < -1):
+            D = -1 * A + 2 * B
+            while (y <= y1):
+                plot(screen, color, x, y)
+                if (D > 0):
+                    x = x - 1
+                    D = D - 2 * A
+                y = y + 1
+                D = D + 2 * B
+
+        else:
+            D = 2 * A - B
+            while (x <= x1):
+                plot(screen, color, x, y)
+                if (D < 0):
+                    y = y - 1
+                    D = D - 2 * B
+                x = x + 1
+                D = D + 2 * A
