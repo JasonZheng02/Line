db5db23c797528d5c46996e3476d2008bdeac2d6
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 01:37:32 -0500

    .

--- draw.py
+++ draw.py
@@ -3,14 +3,16 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     A = y1 - y0
     B = (x1 - x0) * -1
-    D = 2*A + B
     x = x0
     y = y0
+    slope = (A / B) * -1
 
-    while (x<= x1):
-        plot(screen, color, x, y)
-        if (D > 0):
-               y = y + 1
-               D = D + 2 * B
-        x = x + 1
-        D = D + 2 * A
+    if (slope < 1 && slope > 0):
+        D = 2*A + B
+        while (x<= x1):
+            plot(screen, color, x, y)
+            if (D > 0):
+                   y = y + 1
+                   D = D + 2 * B
+            x = x + 1
+            D = D + 2 * A
