0dcc7d706b71cf2b49a8e3ec951f09f43ba0d66d
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 02:27:22 -0500

    .

--- draw.py
+++ draw.py
@@ -38,7 +38,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         while (y <= y1):
             plot(screen, color, x, y)
             if (D > 0):
-                x = x -1 1
+                x = x - 1
                 D = D - 2 * A
             y = y + 1
             D = D + 2 * B
