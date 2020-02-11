7ba3286631d5cd54d69a90af123b16c458b503ae
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 02:26:56 -0500

    .

--- draw.py
+++ draw.py
@@ -34,14 +34,14 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             D = D + 2 * B
 
     elif (slope < -1):
-        D = A - 2 * B
-        while (y >= y1):
+        D = -1 * A + 2 * B
+        while (y <= y1):
             plot(screen, color, x, y)
             if (D > 0):
-                x = x + 1
-                D = D + 2 * A
-            y = y - 1
-            D = D - 2 * B
+                x = x -1 1
+                D = D - 2 * A
+            y = y + 1
+            D = D + 2 * B
 
     else:
         D = 2 * A - B
