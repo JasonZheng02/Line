7e3e8e6c6b5c0427196c8aaa3db3c83737328fc3
Jason Zheng <jzheng02@stuy.edu>
2020-02-11 02:23:36 -0500

    .

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ draw_line(0, 0, XRES-1, YRES-1, s, c)
 draw_line(0, 0, XRES-1, YRES / 2, s, c)
 draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
 
-# #octants 8 and 4
-# c[BLUE] = 255;
-# draw_line(0, YRES-1, XRES-1, 0, s, c);
-# draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-# draw_line(XRES-1, 0, 0, YRES/2, s, c);
+#octants 8 and 4
+c[BLUE] = 255;
+draw_line(0, YRES-1, XRES-1, 0, s, c);
+draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+draw_line(XRES-1, 0, 0, YRES/2, s, c);
 #
 #octants 2 and 6
 c[RED] = 255;
